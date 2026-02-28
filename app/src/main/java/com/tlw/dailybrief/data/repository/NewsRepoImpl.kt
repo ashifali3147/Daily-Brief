61802acaf6166ec6823ace51bc554967a5a097bc
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2026-02-28 19:37:15 +0530

    feat: Implement getAllNews in NewsRepoImpl
    Updates `NewsRepoImpl` to include `NewsDao` as a dependency and implements the `getAllNews` function to fetch news data from the local database.

--- app/src/main/java/com/tlw/dailybrief/data/repository/NewsRepoImpl.kt
+++ app/src/main/java/com/tlw/dailybrief/data/repository/NewsRepoImpl.kt
@@ -4,12 +4,16 @@ import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
+import com.tlw.dailybrief.data.local.NewsDao
 import com.tlw.dailybrief.data.worker.FetchWorker
 import com.tlw.dailybrief.domain.model.News
 import com.tlw.dailybrief.domain.repository.NewsRepo
 import kotlinx.coroutines.flow.Flow
 
-class NewsRepoImpl(private val workManager: WorkManager) : NewsRepo {
+class NewsRepoImpl(
+    private val workManager: WorkManager,
+    private val newsDao: NewsDao
+) : NewsRepo {
 
     override fun getLatestNews() {
         val constraints = Constraints.Builder()
@@ -21,9 +25,7 @@ class NewsRepoImpl(private val workManager: WorkManager) : NewsRepo {
         workManager.enqueue(workRequest)
     }
 
-    override fun getAllNews(): Flow<List<News>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAllNews(): Flow<List<News>> = newsDao.getAllNews()
 
     override fun setPeriodicWorkRequest() {
         TODO("Not yet implemented")
